e71bb1b2479b5501fb0692c533805e9b1bb05bee
yasuo- <breitling1216@gmail.com>
2018-05-22 16:05:08 +0900

    Update: style

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,19 @@ Rails.application.routes.draw do
   }
   resources :reservations
   resources :listings do
-    resources :reservations, only: [:create]
+    resources :reservations, only: [:new, :create]
   end
 
-  get 'manegelisting/:id/basics',   to: 'listings#basics',   as: 'manage_listing_basics'
-  get 'manegelisting/:id/summary',  to: 'listings#summary',  as: 'manage_listing_summary'
-  get 'manegelisting/:id/address',  to: 'listings#address',  as: 'manage_listing_address'
-  get 'manegelisting/:id/price',    to: 'listings#price',    as: 'manage_listing_price'
-  get 'manegelisting/:id/facility', to: 'listings#facility', as: 'manage_listing_facility'
-  get 'manegelisting/:id/photos',   to: 'listings#photos',   as: 'manage_listing_photos'
-  get 'manegelisting/:id/calendar', to: 'listings#calendar', as: 'manage_listing_calendar'
-  get 'manegelisting/:id/map',      to: 'listings#map',      as: 'manage_listing_map'
-  get 'manegelisting/:id/publish',  to: 'listings#publish',  as: 'manage_listing_publish'
+  get 'manegelisting/:id/basics',      to: 'listings#basics',      as: 'manage_listing_basics'
+  get 'manegelisting/:id/summary',     to: 'listings#summary',     as: 'manage_listing_summary'
+  get 'manegelisting/:id/address',     to: 'listings#address',     as: 'manage_listing_address'
+  get 'manegelisting/:id/price',       to: 'listings#price',       as: 'manage_listing_price'
+  get 'manegelisting/:id/facility',    to: 'listings#facility',    as: 'manage_listing_facility'
+  get 'manegelisting/:id/photos',      to: 'listings#photos',      as: 'manage_listing_photos'
+  get 'manegelisting/:id/calendar',    to: 'listings#calendar',    as: 'manage_listing_calendar'
+  get 'managelisting/:id/bankaccount', to: 'listings#bankaccount', as: 'manage_listing_bankaccount'
+  get 'manegelisting/:id/map',         to: 'listings#map',         as: 'manage_listing_map'
+  get 'manegelisting/:id/publish',     to: 'listings#publish',     as: 'manage_listing_publish'
 
   # stripe connect oauth path
   get '/connect/oauth',             to: 'stripe#oauth',       as: 'stripe_oauth'
