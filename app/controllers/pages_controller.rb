cd3964669861783f3d6d1d8b710fbd69f10b55b0
neos-ykurihara <ykurihara@neoscorp.jp>
2018-06-28 18:28:52 +0900

    bugfix: #40

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,8 @@ class PagesController < ApplicationController
 
   def search
     if params[:search].present?
-      @latitude = params["lat"]
-      @longitude = params["lng"]
+      @latitude = latitude(params["lat"], params[:search])
+      @longitude = longitude(params["lng"], params[:search])
       geolocation = [@latitude, @longitude]
       @listings = Listing.actived.near_location(geolocation)
     else
@@ -17,5 +17,19 @@ class PagesController < ApplicationController
       @latitude = @listings.to_a[0].latitude
       @longitude = @listings.to_a[0].longitude
     end
+  end
+
+  private
+
+  def latitude(lat, search_name)
+    return lat if lat.present?
+    locations = Geocoder.coordinates(search_name)
+    locations[0]
+  end
+
+  def longitude(lng, search_name)
+    return lng if lng.present?
+    locations = Geocoder.coordinates(search_name)
+    locations[1]
   end
 end
