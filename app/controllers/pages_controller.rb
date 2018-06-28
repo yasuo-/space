0b9a9626bdd369e448a8d7457464b724328025e8
neos-ykurihara <ykurihara@neoscorp.jp>
2018-06-28 17:01:54 +0900

    Update: #37 map set center

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,13 @@ class PagesController < ApplicationController
   end
 
   def search
+    if params[:search].present?
+      @latitude = params["lat"]
+      @longitude = params["lng"]
+    else
+      @listings = Listing.where(active: true).all
+      @latitude = @listings.to_a[0].latitude
+      @longitude = @listings.to_a[0].longitude
+    end
   end
 end
