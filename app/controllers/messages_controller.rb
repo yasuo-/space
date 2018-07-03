c16588321f099e4bdf199dbff312a27b03531b14
neos-ykurihara <ykurihara@neoscorp.jp>
2018-07-03 09:07:39 +0900

    Add: #34 message base add

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,33 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_converstaion
+
+  def index
+    if current_user == @conversation.sender || current_user == @conversation.recipient
+      @other = current_user == @conversation.sender ? @conversation.recipient : @collect { |e|  }
+      @messages == @conversation.messages.order('created_at DESC')
+    else
+      redirect_to conversations_path, alert: "他人のメッセージにアクセスできません"
+  end
+
+  def create
+    @message = @conversation.message.new(message_params)
+    @messages = @conversation.messages.order("created_at DESC")
+
+    if @message.save
+      respond_to do |format|
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def set_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628233657) do
+ActiveRecord::Schema.define(version: 20180702012459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "home_type", limit: 50
@@ -41,6 +48,16 @@ ActiveRecord::Schema.define(version: 20180628233657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -111,6 +128,8 @@ ActiveRecord::Schema.define(version: 20180628233657) do
   end
 
   add_foreign_key "listings", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "photos", "listings"
   add_foreign_key "reservations", "listings"
   add_foreign_key "reservations", "users"
